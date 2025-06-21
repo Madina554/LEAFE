9a695dc74f195b2d02dc2d3bb03cab0245fb30b7
Madina <madinamuradova554@gmail.com>
2025-06-21 13:41:39 +0500

    yangi ozgarish

--- script.js
+++ script.js
@@ -2,4 +2,18 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
     e.preventDefault();
     alert("Спасибо! Мы скоро свяжемся с вами 🌿");
 });
-  
+
+// Плавное появление при скролле
+const sections = document.querySelectorAll('.section');
+
+function showOnScroll() {
+    sections.forEach(sec => {
+        const rect = sec.getBoundingClientRect();
+        if (rect.top < window.innerHeight - 100) {
+            sec.classList.add('visible');
+        }
+    });
+}
+
+window.addEventListener('scroll', showOnScroll);
+window.addEventListener('load', showOnScroll);
